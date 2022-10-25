8e32f38a2a556871aa4cbfe78787cb5719aad720
Christeena <christeena.s@thoughtworks.com>
2022-10-25 20:16:41 +0530

    Add netflix logo and avatar in header

---
+++ netflix-clone/src/Components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./NavBar.css"
+function NavBar() {
+  return (
+    <div className='navbar'>
+        <img className='logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="logo" />
+        <img className='avatar' src="https://i.pinimg.com/736x/bd/ee/4c/bdee4c328550aaf21aa9f43fd19e2136.jpg" alt="avatar" />
+    </div>
+  )
+}
+
+export default NavBar
