1fb4d3c0dd6537a68ab60882d34eae062dab3309
Christeena <christeena.s@thoughtworks.com>
2022-10-25 23:10:01 +0530

    Refactor NavBar folder for NavBar.css and NavBar.js files

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import NavBar from "./Components/NavBar";
 import "./App.css"
+import NavBar from "./Components/NavBar/NavBar";
 function App() {
   return (
     <div className="App">
