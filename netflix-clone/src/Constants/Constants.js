99d05cec49d35687feef21e4297a262933cece24
Christeena <christeena.s@thoughtworks.com>
2022-11-08 11:28:48 +0530

    Add. movie list  as dynamic'

--- netflix-clone/src/Constants/Constants.js
+++ netflix-clone/src/Constants/Constants.js
@@ -1,3 +1,3 @@
 export const baseUrl='https://api.themoviedb.org/3';
-export const API_KEY='ff71d61aa9f1717c47490539ca1e0ab1';
+export const API_KEY="ff71d61aa9f1717c47490539ca1e0ab1";
 export const imageUrl='https://image.tmdb.org/t/p/original';
