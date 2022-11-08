b42dfdf390d594ac1e245b825dc9d1e0901d5d0d
Christeena <christeena.s@thoughtworks.com>
2022-11-08 15:48:31 +0530

    Add.movies in rows based on genre

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -3,13 +3,16 @@ import "./App.css"
 import Banner from "./Components/Banner/Banner";
 import NavBar from "./Components/NavBar/NavBar";
 import RowPost from "./Components/RowPost/RowPost";
+import {action,orginals,comedy, horror} from "./urls"
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <Banner/>
-      <RowPost/>
-    </div>
+      <RowPost url={orginals} title='Netflix Orginals'/>
+      <RowPost url={action} title='Action Thrillers' isSmall/>
+      <RowPost url={comedy} title='Comedies' isSmall/>
+      <RowPost url={horror} title='Horror Movies' isSmall/>    </div>
   );
 }
 
--- netflix-clone/src/Components/RowPost/RowPost.js
+++ netflix-clone/src/Components/RowPost/RowPost.js
@@ -1,24 +1,24 @@
 import React,{useEffect,useState} from 'react'
 import "./RowPost.css"
 import axios from 'axios'
-import {API_KEY,imageUrl} from '../../Constants/Constants'
-function RowPost() {
+import {imageUrl} from '../../Constants/Constants'
+function RowPost(props) {
   const [movies, setMovies] = useState([])
   useEffect(() => {
-    axios.get(`https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213`).then(response=>{
+    axios.get(props.url).then(response=>{
       console.log(response.data)
       setMovies(response.data.results)
     }).catch(err=>{
-      alert('Network error')
+      //alert('Network error')
     })
   }, [])
   
   return (
     <div className='row'>
-        <h2>Netflix Orginals</h2>
+        <h2>{props.title}</h2>
         <div className='posters'>
             {movies.map((obj)=>
-              <img className='poster' src={`${imageUrl+obj.backdrop_path}`} alt="poster" />
+              <img className={props.isSmall ? 'smallPoster' : 'poster'} src={`${imageUrl+obj.backdrop_path}`} alt="poster" />
             )}
         </div>
     </div>
