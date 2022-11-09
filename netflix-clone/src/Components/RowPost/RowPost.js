4641787e9cd72924e1c3d4c2aeb6caf2f1088dc8
Christeena <christeena.s@thoughtworks.com>
2022-11-09 13:45:09 +0530

    Add.movie trailer

--- netflix-clone/src/Components/RowPost/RowPost.js
+++ netflix-clone/src/Components/RowPost/RowPost.js
@@ -1,10 +1,13 @@
 import React,{useEffect,useState} from 'react'
 import "./RowPost.css"
 import axios from 'axios'
-import {imageUrl} from '../../Constants/Constants'
+import {API_KEY, imageUrl} from '../../Constants/Constants'
+import YouTube from 'react-youtube'
 function RowPost(props) {
   const [movies, setMovies] = useState([])
+  const [urlId, setUrlId] = useState('')
   useEffect(() => {
+    // eslint-disable-next-line
     axios.get(props.url).then(response=>{
       console.log(response.data)
       setMovies(response.data.results)
@@ -12,15 +15,32 @@ function RowPost(props) {
       //alert('Network error')
     })
   }, [])
-  
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+  const handleMovie = (id)=>{
+    console.log(id)
+    axios.get(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then(response=>{
+      if(response.data.results.length!==0){
+        setUrlId(response.data.results[0])
+      }else{
+        console.log('Array Empty')
+      }
+    })
+  }
   return (
     <div className='row'>
         <h2>{props.title}</h2>
         <div className='posters'>
             {movies.map((obj)=>
-              <img className={props.isSmall ? 'smallPoster' : 'poster'} src={`${imageUrl+obj.backdrop_path}`} alt="poster" />
+              <img onClick={()=>handleMovie(obj.id)} className={props.isSmall ? 'smallPoster' : 'poster'} src={`${imageUrl+obj.backdrop_path}`} alt="poster" />
             )}
         </div>
+       {urlId && <YouTube videoId={urlId.key} opts={opts}/>}
     </div>
   )
 }
